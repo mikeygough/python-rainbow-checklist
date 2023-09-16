78c467da2ecc69a9816b51c0743b56f508eac807
mikey <mikey@hey.com>
2023-09-16 14:47:24 -0500

    created helper functions

--- checklist.py
+++ checklist.py
@@ -29,6 +29,37 @@ def list_all_items():
         index += 1
 
 
+def mark_completed(index):
+    checklist[index] = '√' + checklist[index]
+
+
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item: ")
+        create(input_item)
+
+    # Read item
+    elif function_code == "R":
+        item_index = int(user_input("Index Number? "))
+
+        # Remember that item_index must actually exist or our program will crash.
+        print(read(item_index))
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    # Catch all
+    else:
+        print("Unknown Option")
+
+
+def user_input(prompt):
+    user_input = input(prompt)
+    return user_input
+
+
 # TEST
 def test():
     create("purple sox")
@@ -44,6 +75,18 @@ def test():
     print(read(0))
     
     list_all_items()
-
-
+    
+    # Call your new function with the appropriate value
+    select("C")
+    # View the results
+    list_all_items()
+    # Call function with new value
+    select("R")
+    # View results
+    list_all_items()
+    # Continue until all code is run
+    
+    user_value = user_input("Please Enter a value: ")
+    print(user_value)
+    
 test()
