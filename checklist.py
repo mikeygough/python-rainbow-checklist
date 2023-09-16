2e39600d3613b26ac0777f41b2a26509d4eb8e32
mikey <mikey@hey.com>
2023-09-16 14:35:12 -0500

    add list_all_items

--- checklist.py
+++ checklist.py
@@ -1,21 +1,35 @@
 checklist = list()
 
+
 # CREATE
 def create(item):
     checklist.append(item)
-    
+
+
 # READ
 def read(index):
     return checklist[index]
-    
+
+
 # UPDATE
 def update(index, item):
     checklist[index] = item
-    
+
+
 # DESTROY
 def destroy(index):
     checklist.pop(index)
-    
+
+
+# LIST ALL ITEMS
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print("{} {}".format(index, list_item))
+        index += 1
+
+
+# TEST
 def test():
     create("purple sox")
     create("red cloak")
@@ -24,9 +38,12 @@ def test():
     print(read(1))
 
     update(0, "purple socks")
+
     destroy(1)
 
     print(read(0))
-    print(read(1))
+    
+    list_all_items()
+
 
 test()
